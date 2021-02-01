0774e59c993b58a5c837339c9a954c7355429010
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-01 09:02:04 +0600

    template using js

---
+++ template.js
@@ -0,0 +1,12 @@
+const firstName = "leonardo";
+const lastName = "DiCaprio";
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+console.log(fullName);
+const multiLine = "I love javascript" +
+                  "It is very powerful";
+
+const multiLine2 = `I love javascript.
+though i am in a very early stage of learning
+it. I hope I will be good in it one day.`;
+console.log(multiLine2);
