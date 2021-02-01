6f5acd7e3e1c08de3455fbe4d6567302086ee023
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-01 08:44:07 +0600

    first practices

---
+++ letconst.js
@@ -0,0 +1,20 @@
+const hubby = "Kate Winslate";
+// hubby = "Jani na";
+console.log(hubby);
+
+const numbers = [12,45];
+numbers[1] = 88;
+console.log(numbers);
+numbers.push(12);
+numbers = ["jani na","Vallage na", "kisu pari na"];
+const nayok = {
+    name: "Sakib khan", phone: 4578
+};
+// console.log(numbers);
+let patientName = "Rahim Mia";
+patientName = "Fatema Aunty";
+let sum = 0;
+for(let i =0; i<10;i++){
+   sum = sum + i;
+}
+console.log(i);
